666c4e043674f51604a7d8c02438025c10aed5a0
Aquib <aquibshaikh02397@gmail.com>
2020-11-17 14:58:33 +0530

    elapsed sec

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class Timer extends React.Component {
       time: 0,
       x: 0,
       y: 0,
-      timerStr: "00",
+      timerStr: "0",
       ballStyle: { top: "0px", left: "0px" },
     };
     this.timerUpdate = this.timerUpdate.bind(this);
